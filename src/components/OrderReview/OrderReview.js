e8494f25aacdb3977fcc7f9012bd0bea718e2386
Nafis-Hasnat369 <nafis7823@gmail.com>
2021-10-01 01:03:48 +0600

    react router added

--- src/components/OrderReview/OrderReview.js
+++ src/components/OrderReview/OrderReview.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import useProducts from '../../hooks/useProducts';
+import useCart from '../../hooks/useCart';
+import Cart from '../Cart/Cart';
+import ReviewItem from '../ReviewItem/ReviewItem';
+import { deleteFromDb } from '../../utilities/fakedb'
 
 const OrderReview = () => {
+    const [products, setProducts] = useProducts();
+    const [cart, setCart] = useCart(products);
+    const handleRemove = key => {
+        const newCart = cart.filter(product => product.key !== key);
+        setCart(newCart);
+        deleteFromDb(key);
+    };
     return (
-        <div>
-            <h2>This is Order Review</h2>
+        <div className="shop-container">
+            <div className="product-container">
+                {
+                    cart.map(product => <ReviewItem
+                        handleRemove={handleRemove}
+                        product={product}
+                        key={product.key}
+                    ></ReviewItem>)
+                }
+            </div>
+            <div className="cart-container">
+                <Cart cart={cart}></Cart>
+            </div>
         </div>
     );
 };
